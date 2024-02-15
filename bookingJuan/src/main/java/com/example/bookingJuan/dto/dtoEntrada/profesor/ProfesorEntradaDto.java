ac16673df4f644e8662a399b673c4996485bf5d1
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-14 23:15:19 -0300

    validations profesorEntradaDto

--- bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
+++ bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
@@ -1,11 +1,16 @@
 package com.example.bookingJuan.dto.dtoEntrada.profesor;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class ProfesorEntradaDto {
-
+    @NotBlank(message = "Odontologo debe tener nombre")
+    @NotNull(message = "Nombre de odontologo no puede ser nulo")
+    @Size(max = 50,message = "El nombre odontologo debe tener maximo 50 caracteres")
     private String nombre;
 
 }
