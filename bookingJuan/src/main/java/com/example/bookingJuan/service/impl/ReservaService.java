02c6fd8b6c7d817bbc6b74714c9110da98782996
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-15 00:25:05 -0300

    agrego reserva Service

--- bookingJuan/src/main/java/com/example/bookingJuan/service/impl/ReservaService.java
+++ bookingJuan/src/main/java/com/example/bookingJuan/service/impl/ReservaService.java
@@ -1,4 +1,38 @@
 package com.example.bookingJuan.service.impl;
 
-public class ReservaService {
+import com.example.bookingJuan.dto.dtoEntrada.modificacion.ReservaModificacionEntrada;
+import com.example.bookingJuan.dto.dtoEntrada.reserva.ReservaEntradaDto;
+import com.example.bookingJuan.dto.dtoSalida.ReservaSalidaDto;
+import com.example.bookingJuan.exceptions.BadRequestException;
+import com.example.bookingJuan.exceptions.ResourceNotFoundException;
+import com.example.bookingJuan.service.IReservaService;
+
+import java.util.List;
+
+public class ReservaService implements IReservaService {
+
+    @Override
+    public List<ReservaSalidaDto> detallarReservas() {
+        return null;
+    }
+
+    @Override
+    public ReservaSalidaDto crearReserva(ReservaEntradaDto reserva) throws BadRequestException, ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void eliminarReservaPorId(Long id) throws ResourceNotFoundException {
+
+    }
+
+    @Override
+    public ReservaSalidaDto buscarReservaPorId(Long id) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ReservaSalidaDto actualizarReserva(ReservaModificacionEntrada reservaModificacionEntrada) throws ResourceNotFoundException {
+        return null;
+    }
 }
