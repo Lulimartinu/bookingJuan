a3348f2c802ae8a8d914bf3cbe399d40796938e7
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-14 23:08:31 -0300

    agrego clase profesorDto

--- bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
+++ bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
@@ -1,2 +1,11 @@
-package com.example.bookingJuan.dto.dtoEntrada.profesor;public class ProfesorEntradaDto {
+package com.example.bookingJuan.dto.dtoEntrada.profesor;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class ProfesorEntradaDto {
+
+    private String nombre;
+
 }
