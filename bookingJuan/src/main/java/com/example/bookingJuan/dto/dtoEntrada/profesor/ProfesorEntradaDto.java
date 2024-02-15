7b8755ea2eb25891c5dbad8c15a94ba319415fc6
juannmachado1995 <juannmachado1995@gmail.com>
2024-02-14 23:12:34 -0300

    pusheando

--- bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
+++ bookingJuan/src/main/java/com/example/bookingJuan/dto/dtoEntrada/profesor/ProfesorEntradaDto.java
@@ -1,11 +1,18 @@
 package com.example.bookingJuan.dto.dtoEntrada.profesor;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.logging.log4j.message.Message;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class ProfesorEntradaDto {
-
+    @NotBlank(message = "Profesor debe tener nombre")
+    @NotNull(message = "Nombre de profesor no puede ser nulo")
+    @Size(max = 20,message = "El nombre profesor debe tener maximo 20 caracteres")
     private String nombre;
 
 }
